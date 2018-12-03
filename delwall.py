f94a32839abd00fc6b2e7c39d220e8640e5f82bd
Danyasha <daniiltikhomirov1998@yandex.ru>
2018-12-03 09:44:47 +0300

    little changes

--- delwall.py
+++ delwall.py
@@ -5,29 +5,15 @@ wallfilter = {'1':'owner', '2':'others', '3':'all'}.get(wallfilter)
 
 token = input("""
 Get your token here \n
-<<<<<<< HEAD
 https://oauth.vk.com/authorize?client_id=6770459&display=page&redirect_uri=https://oauth.vk.com/blank.html&scope=wall,photos&response_type=token&v=5.52\n
-=======
-https://oauth.vk.com/authorize?client_id=6770459&display=page&redirect_uri=https://oauth.vk.com/blank.html&scope=wall,photos&response_type=token&v=5.52
->>>>>>> d7018730b29d1374df59b2f4d739f2b3530607be
 and put here\n""")
 owner_id = input("Insert your user_ID:\n")
 vk_session = vk_api.VkApi(login=None, password=None, token=token)
 vk = vk_session.get_api()
 posts = True
-posts_id = []
-photos_ids = []
 while posts:
     posts = vk.wall.get(owner_id = owner_id, count = 100, filter = wallfilter).get('items')
     for i in posts:
-<<<<<<< HEAD
         vk.wall.delete(owner_id = owner_id, post_id = i.get('id'))
 print('Done')
 input("press any key\n")
-=======
-        posts_id.append(i.get('id'))
-    for i in posts_id:
-        vk.wall.delete(owner_id = owner_id, post_id = i)
-print('')
-input("press any key")
->>>>>>> d7018730b29d1374df59b2f4d739f2b3530607be
