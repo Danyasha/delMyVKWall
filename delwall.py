b898eaaa361a83f96686a8a4bb09f20bcca25828
Danyasha <daniiltikhomirov1998@yandex.ru>
2018-12-03 08:39:51 +0300

    Добавлен фильтр удаления

--- delwall.py
+++ delwall.py
@@ -1,5 +1,7 @@
 import vk_api
 
+wallfilter = str(input("What to delete?\n1 for your only\n2 for non-your only\n3 for all\n"))
+wallfilter = {'1':'owner', '2':'others', '3':'all'}.get(wallfilter)
 token = input("""
 Get your token here \n
 https://oauth.vk.com/authorize?client_id=6770459&display=page&redirect_uri=https://oauth.vk.com/blank.html&scope=wall&response_type=token&v=5.52\n
@@ -8,12 +10,12 @@ owner_id = input("Insert your user_ID:\n")
 vk_session = vk_api.VkApi(login=None, password=None, token=token)
 vk = vk_session.get_api()
 posts = True
+posts_id = []
 while (posts):
     posts = vk.wall.get(owner_id = owner_id, count = 100, filter = 'owner').get('items')
-    posts_id = []
     for i in posts:
         posts_id.append(i.get('id'))
     for i in posts_id:
         vk.wall.delete(owner_id = owner_id, post_id = i)
-print('Done')
-input()
+print('')
+input("press any key")
