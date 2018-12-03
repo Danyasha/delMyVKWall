d7018730b29d1374df59b2f4d739f2b3530607be
Danyasha <daniiltikhomirov1998@yandex.ru>
2018-12-03 09:06:47 +0300

    Update delwall.py

--- delwall.py
+++ delwall.py
@@ -4,7 +4,7 @@ wallfilter = str(input("What to delete?\n1 for your only\n2 for non-your only\n3
 wallfilter = {'1':'owner', '2':'others', '3':'all'}.get(wallfilter)
 token = input("""
 Get your token here \n
-https://oauth.vk.com/authorize?client_id=6770459&display=page&redirect_uri=https://oauth.vk.com/blank.html&scope=wall&response_type=token&v=5.52\n
+https://oauth.vk.com/authorize?client_id=6770459&display=page&redirect_uri=https://oauth.vk.com/blank.html&scope=wall,photos&response_type=token&v=5.52
 and put here\n""")
 owner_id = input("Insert your user_ID:\n")
 vk_session = vk_api.VkApi(login=None, password=None, token=token)
